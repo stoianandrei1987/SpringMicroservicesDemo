11a958365bb0a8da1be0d7a92b9d10e3fcff31a5
Andrei Stoian <stoianandrei980@yahoo.com>
2021-05-28 16:42:34 +0300

    first commit

---
+++ src/test/java/ro/andreistoian/SpringMicroservicesDemo/SpringMicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.andreistoian.SpringMicroservicesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
